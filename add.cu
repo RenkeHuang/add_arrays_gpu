554067d572772e22117f800f9c1e2c419f2262eb
Renke <38452685+RenkeHuang@users.noreply.github.com>
2024-11-03 11:40:32 -0500

    Add cuda implementations of vector addition

---
+++ add.cu
@@ -0,0 +1,48 @@
+/*
+Single-threaded CUDA program that performs vector addition of two arrays.
+*/
+
+#include <math.h>
+#include <iostream>
+
+// Kernel function to add the elements of two arrays
+__global__ void add(int n, float *x, float *y)
+{
+    for (int i = 0; i < n; i++) y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+    int N = 1 << 20;
+    float *x, *y;
+
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++)
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // set execution configuration
+    // Run kernel on 1M elements on the GPU
+    add<<<1, 1>>>(N, x, y);
+
+    // Wait for GPU to finish before accessing results on host
+    // since CUDA kernel launches don’t block the calling CPU thread.
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++) maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
